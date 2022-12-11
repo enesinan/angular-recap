c260966c62b6ea172d0ed3043efa7a5b5168a2b3
enes.inan <enes.inan@vbt.com.tr>
2022-12-11 14:15:12 +0300

    Mouse events with HostListener impl

--- src/app/basic-highlight/basic-highlight.directive.ts
+++ src/app/basic-highlight/basic-highlight.directive.ts
@@ -5,6 +5,7 @@ import { Directive, ElementRef, OnInit } from "@angular/core";
 
 })
 export class BasicHighlightDirective implements OnInit {
+
     constructor(private elementRef: ElementRef){
 
     }
